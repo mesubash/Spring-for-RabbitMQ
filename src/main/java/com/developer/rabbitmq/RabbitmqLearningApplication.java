d25f738b61c1f8ad772d77abebbb55383e73734a
Subash Singh Dhami <subashdhamee@gmail.com>
2025-12-14 16:36:03 +0545

    add initial config

---
+++ src/main/java/com/developer/rabbitmq/RabbitmqLearningApplication.java
@@ -0,0 +1,13 @@
+package com.developer.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqLearningApplication.class, args);
+    }
+
+}
