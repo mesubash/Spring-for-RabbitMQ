dd6db3804bc97a4f37885f6117467c6159b290d5
Subash Singh Dhami <subashdhamee@gmail.com>
2025-12-14 17:07:05 +0545

    add simple message producer which sents message to the exchanged usign the routing key

---
+++ src/main/java/com/developer/rabbitmq/publisher/RabbitMQProducer.java
@@ -0,0 +1,32 @@
+package com.developer.rabbitmq.publisher;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+
+        LOGGER.info("Message to RabbitMQ: {}", message);
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
