8d08495cd5f13335d2c3955754e0576299eb926d
Subash Singh Dhami <subashdhamee@gmail.com>
2025-12-15 14:01:16 +0545

    endpont to send the json message

--- src/main/java/com/developer/rabbitmq/controller/MessageController.java
+++ src/main/java/com/developer/rabbitmq/controller/MessageController.java
@@ -1,20 +1,21 @@
 package com.developer.rabbitmq.controller;
 
+import com.developer.rabbitmq.dto.User;
+import com.developer.rabbitmq.publisher.RabbitMQJsonProducer;
 import com.developer.rabbitmq.publisher.RabbitMQProducer;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/messages")
 public class MessageController {
 
     private final RabbitMQProducer rabbitMQProducer;
+    private final RabbitMQJsonProducer rabbitMQJsonProducer;
 
-    public MessageController(RabbitMQProducer rabbitMQProducer) {
+    public MessageController(RabbitMQProducer rabbitMQProducer, RabbitMQJsonProducer rabbitMQJsonProducer) {
         this.rabbitMQProducer = rabbitMQProducer;
+        this.rabbitMQJsonProducer = rabbitMQJsonProducer;
     }
 
 
@@ -22,5 +23,11 @@ public class MessageController {
     public ResponseEntity<String> sendMessage(@RequestParam("message") String message) {
         rabbitMQProducer.sendMessage(message);
         return ResponseEntity.ok("Message sent to RabbitMQ: " + message);
+    }
+
+    @PostMapping("/json/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user) {
+        rabbitMQJsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("JSON Message sent to RabbitMQ: " + user);
     }
 }
